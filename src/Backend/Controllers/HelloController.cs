2d3a95dccd0db08a361112919ad5f73124582f1a
Michal Dymel <michal@dymel.pl>
2016-10-22 15:07:57 +0200

    Adding mvc and first api controller

---
+++ src/Backend/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class HelloController : Controller
+    {
+        [Route("/api/hello")]
+        public string Hello()
+        {
+            return "Hello World";
+        }
+    }
+}
--- src/Backend/Startup.cs
+++ src/Backend/Startup.cs
@@ -16,6 +16,7 @@ namespace Backend
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +29,7 @@ namespace Backend
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
